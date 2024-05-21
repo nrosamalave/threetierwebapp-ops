4137709183f559c1f60970b058c3e478da9a5bc5
nrosamalave <nrosamalave@gmail.com>
2024-05-21 00:01:22 -0400

    initial commit, vpc, public-web subnet test

---
+++ providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  access_key = var.apikey
+  secret_key = var.secretkey
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "apikey" {
+  description = "aws api key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secretkey" {
+  description = "aws secret key"
+  type        = string
+  sensitive   = true
+}
