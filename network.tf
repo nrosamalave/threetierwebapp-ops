ef6af6ceaa82aded6f7a68386aff4e9a607799e7
nrosamalave <nrosamalave@gmail.com>
2024-05-21 00:11:25 -0400

    testing for_each loop

--- network.tf
+++ network.tf
@@ -7,8 +7,9 @@ resource "aws_vpc" "my-project-vpc" {
 
 # Subnets
 resource "aws_subnet" "my-public-web-subnet" {
-  for_each          = local.subnet
+  for_each          = local.subnet.cidr
   vpc_id            = aws_vpc.my-project-vpc[each.key].id
-  cidr_block        = each.cidr.value
-  availability_zone = each.availability_zones.value
+  cidr_block        = each.value
+  for_each          = local.subnet.availability_zones
+  availability_zone = each.value
 }
