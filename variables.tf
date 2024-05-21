7724ab8c2fbc85c13a6fe28e3d26b6846f55c052
nrosamalave <nrosamalave@gmail.com>
2024-05-21 00:26:30 -0400

    testing region variable

--- variables.tf
+++ variables.tf
@@ -9,3 +9,12 @@ variable "secretkey" {
   type        = string
   sensitive   = true
 }
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  validation {
+    condition     = contains(["us-east-1", "us-east-2"], var.region)
+    error_message = "Invalid AWS region: us-east-1, us-east-2"
+  }
+}
