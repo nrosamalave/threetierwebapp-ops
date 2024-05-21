fc448827b6fc54e8da291e374fdb3f63b91041ed
nrosamalave <nrosamalave@gmail.com>
2024-05-21 10:09:29 -0400

    created subnets, revised locals

--- workspace.tf
+++ workspace.tf
@@ -2,14 +2,63 @@ locals {
     vpc = {
         cidr = "172.20.0.0/20"
     }
-    subnet = {
-        availability_zones = {
-            default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+    subnet = {              
+        availability_zones = {      //local for each az referenced in network.tf
+            useasta = {
+                az = "us-east-1a"
+            }
+
+            useastb = {
+                az = "us-east-1b"
+            }
+
+            useastc = {
+                az = "us-east-1c"
+            }
+
         }
-        cidr = {
-            public-web = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-            private-app = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
-            private-db = ["172.20.7.0/24", "172.20.8.0/24", "172.20.9.0/24"]
+        cidr = {                            //local for each cidr referenced in each subnet in network.tf
+            public-web = { 
+                one   = {
+                    block = "172.20.1.0/24"
+                }
+
+                two   = {
+                    block = "172.20.2.0/24"
+                }
+
+                three   = {
+                    block = "172.20.3.0/24"
+                }
+            }
+            
+            private-app = {
+                one   = {
+                    block = "172.20.4.0/24"
+                }
+
+                two   = {
+                    block = "172.20.5.0/24"
+                }
+
+                three   = {
+                    block = "172.20.6.0/24"
+                }
+            }
+
+            private-db = {
+                one   = {
+                    block = "172.20.7.0/24"
+                }
+
+                two   = {
+                    block = "172.20.8.0/24"
+                }
+
+                three   = {
+                    block = "172.20.9.0/24"
+                }
+            } 
         }
     }
 }
