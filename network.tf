07ea810c6eed2442b736a309a1701069a317ac71
nrosamalave <nrosamalave@gmail.com>
2024-05-21 00:04:50 -0400

    adding vpc and subnets

--- network.tf
+++ network.tf
@@ -1,14 +1,14 @@
 # VPC
-#resource "aws_vpc" "my-project-vpc" {
-#  for_each         = local.vpc 
-#  cidr_block       = each.value
-#  instance_tenancy = "default"
-#}
+resource "aws_vpc" "my-project-vpc" {
+  for_each         = local.vpc 
+  cidr_block       = each.value
+  instance_tenancy = "default"
+}
 
 # Subnets
-#resource "aws_subnet" "my-public-web-subnet" {
-#  for_each          = local.subnet
-#  vpc_id            = aws_vpc.my-project-vpc.id
-#  cidr_block        = each.cidr.value
-#  availability_zone = each.availability_zones.value
-#}
+resource "aws_subnet" "my-public-web-subnet" {
+  for_each          = local.subnet
+  vpc_id            = aws_vpc.my-project-vpc.id
+  cidr_block        = each.cidr.value
+  availability_zone = each.availability_zones.value
+}
