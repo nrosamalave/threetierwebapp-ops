30c8633cf5a4bd22c7916f587dc69e5590a30958
nrosamalave <nrosamalave@gmail.com>
2024-05-21 21:00:00 -0400

    vpc and public web subnets

--- main.tf
+++ main.tf
@@ -1,50 +1,19 @@
-# VPC definition
-resource "aws_vpc" "this" {
-  cidr_block = local.vpc_cidr_block
+# Create VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = local.vpc_cidr
+
+  tags = {
+    Name = "my-vpc"
+  }
 }
 
-# Subnet definition
-resource "aws_subnet" "this" {
-  for_each = { for s in local.flattened_subnets : s.key => s }
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = each.value.cidr_block
+resource "aws_subnet" "public-web-subnets" {
+  for_each   = local.subnets
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = each.value.cidr
   availability_zone = each.value.az
+
   tags = {
-    Name = each.value.key
+    Name = "public-web-subnets"
   }
-}
-
-# Route Table definition
-resource "aws_route_table" "this" {
-  for_each = toset(local.route_tables)
-  vpc_id = aws_vpc.this.id
-  tags = {
-    Name = "${each.key}-route_table"
-  }
-}
-
-# Route definition
-resource "aws_route" "this" {
-  for_each = local.route_tables
-  route_table_id         = aws_route_table.this[each.key].id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-}
-
-# Internet Gateway definition
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-}
-
-# Associate Route Tables with Subnets
-resource "aws_route_table_association" "this" {
-  for_each = { 
-    for s in local.flattened_subnets :
-    "${s.az}-${s.name}" => {
-      subnet_id = aws_subnet.this[s.key].id
-      route_table_id = aws_route_table.this[split(s.name, "_")[1]].id
-    }
-  }
-  subnet_id      = each.value.subnet_id
-  route_table_id = each.value.route_table_id
 }
--- workspace.tf
+++ workspace.tf
@@ -1,34 +1,21 @@
 locals {
-  vpc_cidr_block = "10.0.0.0/16"
-  
-  az_subnets = {
-    "us-east-1a" = [
-      { name = "private_db", cidr = "10.0.1.0/24" },
-      { name = "public_web", cidr = "10.0.2.0/24" },
-      { name = "private_app", cidr = "10.0.3.0/24" },
+  vpc_cidr = "10.0.0.0/20"
+
+  subnets = {
+    web = [
+      { cidr = "10.0.1.0/24", az = "us-east-1a" },
+      { cidr = "10.0.2.0/24", az = "us-east-1b" },
+      { cidr = "10.0.3.0/24", az = "us-east-1c" },
     ]
-    "us-east-1b" = [
-      { name = "private_db", cidr = "10.0.4.0/24" },
-      { name = "public_web", cidr = "10.0.5.0/24" },
-      { name = "private_app", cidr = "10.0.6.0/24" },
+    app = [
+      { cidr = "10.0.4.0/24", az = "us-east-1a" },
+      { cidr = "10.0.5.0/24", az = "us-east-1b" },
+      { cidr = "10.0.6.0/24", az = "us-east-1c" },
     ]
-    "us-east-1c" = [
-      { name = "private_db", cidr = "10.0.7.0/24" },
-      { name = "public_web", cidr = "10.0.8.0/24" },
-      { name = "private_app", cidr = "10.0.9.0/24" },
+    db = [
+      { cidr = "10.0.7.0/24", az = "us-east-1a" },
+      { cidr = "10.0.8.0/24", az = "us-east-1b" },
+      { cidr = "10.0.9.0/24", az = "us-east-1c" },
     ]
   }
-
-  flattened_subnets = flatten([
-    for az, subnets in local.az_subnets : [
-      for subnet in subnets : {
-        az         = az
-        name       = subnet.name
-        cidr_block = subnet.cidr
-        key        = "${az}-${subnet.name}"
-      }
-    ]
-  ])
-
-  route_tables = ["web", "db", "app"]
 }
