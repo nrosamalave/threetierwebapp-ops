7771880b1dbe17e357a429e516534fb884b9e4e8
nrosamalave <nrosamalave@gmail.com>
2024-05-22 13:37:12 -0400

    testing

--- main.tf
+++ main.tf
@@ -83,7 +83,8 @@ resource "aws_route_table" "web" {
   vpc_id = aws_vpc.my-vpc.id
 
   route = {
-    cidr_block = 0.0.0.0/0
+    type = string
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my-igw.id
   }
 
