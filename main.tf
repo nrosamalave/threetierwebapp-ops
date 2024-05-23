f409bc327e24ca950ceb3a76e12e732cf0d4f6a9
nrosamalave <nrosamalave@gmail.com>
2024-05-22 23:57:16 -0400

    editing jumpserver sg ingress for ssh

--- main.tf
+++ main.tf
@@ -154,6 +154,14 @@ resource "aws_security_group" "jump-server" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.jump-server.id
+  cidr_ipv4         = 174.179.163.53/32
+  from_port         = 22
+  ip_protocol       = "ssh"
+  to_port           = 22
+}
+
 # Key Pair
 resource "aws_key_pair" "aws-key" {
   key_name   = "aws-key"
