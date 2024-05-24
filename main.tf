6b797bc5998179f4edd92799bc190be55b6b7005
nrosamalave <nrosamalave@gmail.com>
2024-05-24 14:53:59 -0400

    egress traffic from php server

--- main.tf
+++ main.tf
@@ -181,10 +181,27 @@ resource "aws_security_group" "php-sg" {
     security_groups = [aws_security_group.jump-server.id]
   }
 
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = 0.0.0.0/0
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = 0.0.0.0/0
+  }
+
   tags = {
     Name = "my-php-sg"
   }
 }
+
+
+
 
 # Key Pair
 
