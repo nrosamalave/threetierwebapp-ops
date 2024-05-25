71761e7fbecd64bf0a6005df383a43f326439aeb
nrosamalave <nrosamalave@gmail.com>
2024-05-25 17:22:20 -0400

    fixed line 308, does not have id

--- main.tf
+++ main.tf
@@ -305,7 +305,7 @@ resource "aws_lb_target_group" "php-target-group" {
 resource "aws_lb_target_group_attachment" "php-targets" {
   for_each          = local.php_app_instances
   target_group_arn  = aws_lb_target_group.php-target-group.arn
-  target_id         = each.value.id
+  target_id         = each.value
   port              = 80
 }
 
