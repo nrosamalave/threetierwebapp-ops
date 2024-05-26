938458e1accd4e6c74b7882ebdbda6a8911d2972
nrosamalave <nrosamalave@gmail.com>
2024-05-25 20:10:23 -0400

    added egress to php-sg to rds-sg

--- main.tf
+++ main.tf
@@ -203,6 +203,13 @@ resource "aws_security_group" "php-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.rds-sg.id]
+  }
+
   tags = {
     Name = "my-php-sg"
   }
